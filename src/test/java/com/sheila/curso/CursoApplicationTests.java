3bab845d3d4ba47fb36aab150d085641ba55fc94
Sheila Schlickmann <sheila31sl@hotmail.com>
2021-05-12 21:13:14 -0300

    Projeto Criado

---
+++ src/test/java/com/sheila/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheila.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
