9757d95e446f5c4bd560b6cb1ffd7211741c6238
Sheila Schlickmann <sheila31sl@hotmail.com>
2021-05-12 22:24:14 -0300

    H2 database, test profile, JPA

--- src/main/java/com/sheila/curso/entities/User.java
+++ src/main/java/com/sheila/curso/entities/User.java
@@ -2,20 +2,27 @@ package com.sheila.curso.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
-	
-	public User() {
-		
-	}
 
+	public User() {
+
+	}
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -26,56 +33,45 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPhone() {
 		return phone;
 	}
-
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -84,7 +80,6 @@ public class User implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -102,6 +97,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
 
 }
