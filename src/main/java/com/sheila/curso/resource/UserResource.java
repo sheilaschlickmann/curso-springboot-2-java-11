55ccb2a6f811d76c0e8ae7c52c35b4c94d2e74ea
Sheila Schlickmann <sheila31sl@hotmail.com>
2021-05-12 22:02:18 -0300

    User entity and resource

---
+++ src/main/java/com/sheila/curso/resource/UserResource.java
@@ -0,0 +1,20 @@
+package com.sheila.curso.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sheila.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User (1L, "Ana","ana@gmail.com","87878787","3265" );
+		return ResponseEntity.ok().body(user);
+	}
+
+}
