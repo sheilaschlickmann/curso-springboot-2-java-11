7d77fcf889a0cf3b851a1e8a7034fd42d7acd495
Sheila Schlickmann <sheila31sl@hotmail.com>
2021-05-29 17:42:10 -0300

    Exception handling - delete

--- src/main/java/com/sheila/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/sheila/course/resources/exceptions/ResourceExceptionHandler.java
@@ -23,5 +23,16 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseNotFound(DatabaseException e, HttpServletRequest request) {
+
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),	request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+
+	}
 
 }
+
--- src/main/java/com/sheila/course/services/UserService.java
+++ src/main/java/com/sheila/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.sheila.course.entities.User;
 import com.sheila.course.repositories.UserRepository;
+import com.sheila.course.resources.exceptions.DatabaseException;
 import com.sheila.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -23,15 +26,21 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
